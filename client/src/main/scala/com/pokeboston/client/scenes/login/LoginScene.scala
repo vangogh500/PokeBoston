3b7b71ed77d3854e042f785a3dccfce6ac46d553
Kai Matsuda <matsudaskai@gmail.com>
2018-07-27 01:35:13 -0400

    login page ui

--- client/src/main/scala/com/pokeboston/client/scenes/login/LoginScene.scala
+++ client/src/main/scala/com/pokeboston/client/scenes/login/LoginScene.scala
@@ -15,11 +15,25 @@ object LoginScene {
   case class State(progress: Int = 0)
   private val component = ScalaComponent.static("LoginScene")(
     <.div(^.className := "w-100 h-100 d-flex flex-column", VdomStyle("background") := "#fafafa")(
-      <.div(^.className := "d-flex justify-content-center", VdomStyle("margin") := "50px 0px")(
-        <.img(^.src := "https://fontmeme.com/permalink/180722/3fab27004af3568242bac9c333f8ffce.png", ^.height := "50px")
-      ),
+      <.img(
+        ^.className := "ml-auto mr-auto",
+        ^.src := "https://fontmeme.com/permalink/180722/3fab27004af3568242bac9c333f8ffce.png",
+        ^.height := "50px",
+        VdomStyle("margin") := "25px 0px"),
       <.div(^.className := "container")(
+        <.ul(^.className := "nav nav-pills nav-justified justify-content-center bg-white", VdomStyle("margin-bottom") := "25px")(
+          <.li(^.className := "nav-item")(
+            <.a(^.className := "nav-link active")("Login")
+          ),
+          <.li(^.className := "nav-item")(
+            <.a(^.className := "nav-link")("Signup")
+          )
+        ),
         HorizontalProgressWithBreadCrumbs(0, Seq("1", "2", "3")),
+        <.div(VdomStyle("margin") := "40px 0px 50px 0px")(
+          <.h4(^.className := "display-4 text-center")("Choose a Passport"),
+          <.hr()
+        ),
         <.div(^.className := "d-flex flex-wrap justify-content-around")(
           Seq(
             "https://upload.wikimedia.org/wikipedia/commons/f/ff/Facebook_logo_36x36.svg",
@@ -27,7 +41,9 @@ object LoginScene {
             "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg",
             "https://upload.wikimedia.org/wikipedia/commons/5/5f/Windows_logo_-_2012.svg"
           ).toVdomArray { imgUrl =>
-            <.div(^.className := "col-sm-3 d-flex justify-content-center")(
+            <.div(
+              ^.className := "col-md-4 col-lg-3 d-flex justify-content-center",
+              VdomStyle("margin-top") := "30px")(
               <.div(
                 ^.className := "card shadow",
                 VdomStyle("width") := "80%",
@@ -41,18 +57,12 @@ object LoginScene {
                 )))
           }
         ),
-        <.div(^.className := "d-flex justify-content-center align-items-center position-relative", VdomStyle("margin") := "30px 0px")(
-          <.hr(^.className := "position-absolute w-100", VdomStyle("margin") := "0"),
-          <.div(VdomStyle("background") := "#fafafa", AppLayout.zindex(10))(
-            <.h4(^.className := "display-4")("OR")
-          )
-        ),
-        <.div(^.className := "d-flex flex-wrap justify-content-around")(
+        <.div(^.className := "d-flex flex-wrap justify-content-around", VdomStyle("margin-top") := "30px")(
           Seq(
             "mdi-email",
             "mdi-cellphone-android"
           ).toVdomArray { logo =>
-            <.div(^.className := "col-sm-3 d-flex justify-content-center")(
+            <.div(^.className := "col-md-4 col-lg-3 d-flex justify-content-center")(
               <.div(
                 ^.className := "card shadow",
                 VdomStyle("width") := "80%",
